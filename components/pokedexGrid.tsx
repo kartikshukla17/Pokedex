fb3eeb6f3389a18e45b32da5a695bb845c411d0e
Kartik Shukla <kartikconnect17@gmail.com>
2024-12-01 06:23:27 +0530

    Update pokedexGrid.tsx

--- components/pokedexGrid.tsx
+++ components/pokedexGrid.tsx
@@ -133,7 +133,7 @@ const PokedexGrid: React.FC = () => {
           />
         </div>
 
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-6">
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-6 justify-items-center">
           {filteredPokemonData.map((pokemon) => (
             <PokemonCard
               key={pokemon.id}
